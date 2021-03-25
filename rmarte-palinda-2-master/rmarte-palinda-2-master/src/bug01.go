ddcf999047bf359286c85a8c8239f44e25610cb8
Markus Videfors <markus.videfors@outlook.com>
2021-03-25 23:04:10 +0100

    fix bugs

--- rmarte-palinda-2-master/rmarte-palinda-2-master/src/bug01.go
+++ rmarte-palinda-2-master/rmarte-palinda-2-master/src/bug01.go
@@ -4,7 +4,14 @@ import "fmt"
 
 // I want this program to print "Hello world!", but it doesn't work.
 func main() {
-	ch := make(chan string)
+    // The code doesn't work because ch is an unbuffered channel. Therefore it requires a reciever to exist when sending to the channel.
+    // OLD
+	// ch := make(chan string)
+    // By replacing it with a buffered channel we can send to the channel without having a reciever ready
+    // NEW
+    ch := make(chan string, 1)
+    //
+
 	ch <- "Hello world!"
 	fmt.Println(<-ch)
 }
--- rmarte-palinda-2-master/rmarte-palinda-2-master/src/bug02.go
+++ rmarte-palinda-2-master/rmarte-palinda-2-master/src/bug02.go
@@ -6,20 +6,24 @@ import (
 )
 
 // This program should go to 11, but it seemingly only prints 1 to 10.
+// After completing the loop the main function closes the channel and is then complete. When the main goroutine ends all other running goroutines are cancelled. To fix this we need to make sure that main does not close until all other goroutines have completed. Since we only have one goroutine this can easily be achieved by giving Print another channel to which it can send a message when done. Since recieving from a channel blocks further execution until data is recieved main will not terminate until it has recieved a message.
 func main() {
-	ch := make(chan int)
-	go Print(ch)
+	ch, done := make(chan int), make(chan bool)
+	go Print(ch, done) // fix
 	for i := 1; i <= 11; i++ {
 		ch <- i
 	}
 	close(ch)
+
+    <-done
 }
 
 // Print prints all numbers sent on the channel.
 // The function returns when the channel is closed.
-func Print(ch <-chan int) {
+func Print(ch <-chan int, done chan bool) {
 	for n := range ch { // reads from channel until it's closed
 		time.Sleep(10 * time.Millisecond) // simulate processing time
 		fmt.Println(n)
 	}
+    done <- true
 }
